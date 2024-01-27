449357f2b59708ca844098470c10c6ef623f1f75
uumair327 <mohdumair.a@somaiya.edu>
2024-01-27 21:09:48 +0530

    Scanner added

--- lib/screens/scanner.dart
+++ lib/screens/scanner.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import 'package:marathon/utils/constants.dart';
+import 'package:qr_bar_code_scanner_dialog/qr_bar_code_scanner_dialog.dart';
 
 class ScannerPage extends StatelessWidget {
-  const ScannerPage({super.key});
+  ScannerPage({super.key});
+  final _qrBarCodeScannerDialogPlugin = QrBarCodeScannerDialog();
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _openQRScanner(context);
+    });
+
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -17,29 +23,36 @@ class ScannerPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
-                  child: Text("00:00:00", style: TextStyle(color: kWhite, fontSize: 20),),
+                  child: const Text(
+                    "00:00:00",
+                    style: TextStyle(color: kWhite, fontSize: 20),
+                  ),
                   padding: const EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                    color: kRed,
-                    borderRadius: BorderRadius.circular(10)
-                  ),
+                      color: kRed, borderRadius: BorderRadius.circular(10)),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      margin: EdgeInsets.symmetric(vertical: 0, horizontal: 5),
-                      child: CircleAvatar(
-                        backgroundColor: kLightRed,
-                        child:Icon(LucideIcons.flame,color: kRed,)
-                      ),
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 0, horizontal: 5),
+                      child: const CircleAvatar(
+                          backgroundColor: kLightRed,
+                          child: Icon(
+                            LucideIcons.flame,
+                            color: kRed,
+                          )),
                     ),
                     Container(
-                      margin: EdgeInsets.symmetric(vertical: 0, horizontal: 5),
-                      child: CircleAvatar(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 0, horizontal: 5),
+                      child: const CircleAvatar(
                           backgroundColor: kLightRed,
-                          child:Icon(LucideIcons.flame,color: kRed,)
-                      ),
+                          child: Icon(
+                            LucideIcons.flame,
+                            color: kRed,
+                          )),
                     ),
                   ],
                 )
@@ -47,17 +60,39 @@ class ScannerPage extends StatelessWidget {
             ),
             Expanded(
               child: Container(
-                margin: EdgeInsets.all(40),
-                child: Center(child: Text("Scanner")),
+                margin: const EdgeInsets.all(40),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: kLightYellow
-                ),
+                    borderRadius: BorderRadius.circular(10),
+                    color: kLightYellow),
+                child: InkWell(onTap: () {
+                  _qrBarCodeScannerDialogPlugin.getScannedQrBarCode(
+                    context: context,
+                    onCode: (scannedCode) {
+                      // setState(() {
+                      //   this.code = scannedCode;
+                      //   // Update checkpoint logic
+                      // });
+                    },
+                  );
+                }),
               ),
             )
           ],
         ),
-      )
+      ),
+    );
+  }
+
+  void _openQRScanner(BuildContext context) {
+    _qrBarCodeScannerDialogPlugin.getScannedQrBarCode(
+      context: context,
+      onCode: (scannedCode) {
+        // Handle the scanned code as needed
+        // setState(() {
+        //   this.code = scannedCode;
+        //   // Update checkpoint logic
+        // });
+      },
     );
   }
 }
