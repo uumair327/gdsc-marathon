5b3248c78e8e068a00661b5cbcc07e50c2253763
uumair327 <mohdumair.a@somaiya.edu>
2024-02-15 21:06:46 +0530

    StopWatch Added with working and minor changes

--- lib/screens/scanner.dart
+++ lib/screens/scanner.dart
@@ -3,15 +3,48 @@ import 'package:lucide_icons/lucide_icons.dart';
 import 'package:marathon/screens/quizPage.dart';
 import 'package:marathon/utils/constants.dart';
 import 'package:qr_bar_code_scanner_dialog/qr_bar_code_scanner_dialog.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 
-class ScannerPage extends StatelessWidget {
+class ScannerPage extends StatefulWidget {
   ScannerPage({Key? key}) : super(key: key);
 
+  @override
+  State<ScannerPage> createState() => _ScannerPageState();
+}
+
+class _ScannerPageState extends State<ScannerPage> {
+  final stopwatch = StopWatchTimer(onChange: (value) {
+    final displayTime = StopWatchTimer.getDisplayTime(value);
+  });
+  bool marathonStarted = false;
   final _qrBarCodeScannerDialogPlugin = QrBarCodeScannerDialog();
+
+  String _printDuration(Duration duration) {
+    String milliseconds = (duration.inMilliseconds % 1000)
+        .toString()
+        .padLeft(2, "0"); // this one for the miliseconds
+    String seconds = ((duration.inMilliseconds ~/ 1000) % 60)
+        .toString()
+        .padLeft(2, "0"); // this is for the second
+    String minutes =
+        ((duration.inMilliseconds ~/ 1000) ~/ 60).toString().padLeft(2, "0");
+    return "$minutes : $seconds : $milliseconds";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await stopwatch.dispose(); // Need to call dispose function.
+  }
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
       _openQRScanner(context);
     });
 
@@ -19,75 +52,120 @@ class ScannerPage extends StatelessWidget {
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: kRed, borderRadius: BorderRadius.circular(10)),
-                    child: const Text(
-                      "00:00:00",
-                      style: TextStyle(color: kWhite, fontSize: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: kRed, borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Timer:',
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          ),
+                          StreamBuilder<int>(
+                            stream: stopwatch.rawTime,
+                            initialData: stopwatch.rawTime.value,
+                            builder: (context, snap) {
+                              final value = snap.data!;
+                              final displayTime =
+                                  StopWatchTimer.getDisplayTime(value);
+                              return Column(
+                                children: <Widget>[
+                                  Text(
+                                    displayTime,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 23,
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 0, horizontal: 5),
-                        child: const CircleAvatar(
-                            backgroundColor: kLightRed,
-                            child: Icon(
-                              LucideIcons.flame,
-                              color: kRed,
-                            )),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 0, horizontal: 5),
-                        child: const CircleAvatar(
-                            backgroundColor: kLightRed,
-                            child: Icon(
-                              LucideIcons.flame,
-                              color: kRed,
-                            )),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(40),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: kLightYellow),
-                  child: InkWell(onTap: () {
-                    _qrBarCodeScannerDialogPlugin.getScannedQrBarCode(
-                      context: context,
-                      onCode: (scannedCode) {
-                        // Check the scanned code value
-                        if (scannedCode == "GDSC: Check Point 2") {
-                          // Navigate to QuizPage if the scanned code matches
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const QuizPage()),
-                          );
-                        } else {
-                          // Handle other scanned code values
-                        }
-                      },
-                    );
-                  }),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 0, horizontal: 5),
+                          child: const CircleAvatar(
+                              backgroundColor: kLightRed,
+                              child: Icon(
+                                LucideIcons.flame,
+                                color: kRed,
+                              )),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 0, horizontal: 5),
+                          child: const CircleAvatar(
+                              backgroundColor: kLightRed,
+                              child: Icon(
+                                LucideIcons.flame,
+                                color: kRed,
+                              )),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-              )
-            ],
+                Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(40),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: kLightYellow),
+                      child: IconButton(
+                        onPressed: () {
+                          _qrBarCodeScannerDialogPlugin.getScannedQrBarCode(
+                            context: context,
+                            onCode: (scannedCode) {
+                              // Check the scanned code value
+                              if (scannedCode == "GDSC: Check Point 2") {
+                                // Navigate to QuizPage if the scanned code matches
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const QuizPage()),
+                                );
+                              } else {
+                                // Handle other scanned code values
+                              }
+                            },
+                          );
+                        },
+                        icon: const Icon(Icons.qr_code),
+                        iconSize: 64,
+                        color: kLightRed,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          marathonStarted = true;
+                          stopwatch.onStartTimer();
+                        });
+                      },
+                      child: const Text(
+                        'Start Marathon',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
